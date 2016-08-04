8e68166569dba5c6a24ec0bd403f535a849632b0
Tanner Welsh <tannerwelsh@gmail.com>
2016-08-04 14:22:55 -0400

    Move utility functions to own file

--- stats.rb
+++ stats.rb
@@ -1,18 +1,8 @@
 require 'csv'
 
 require_relative './game_data.rb'
+require_relative './utils.rb'
 
-module Aggregates
-  def mean(nums)
-    nums.reduce(:+) / nums.count.to_f
-  end
-end
-
-class Numeric
-  def to_percent(limit, decimal = 2)
-    ((self / limit.to_f) * 100).round(decimal)
-  end
-end
 
 class InvalidHoursValueError < StandardError; end
 class MissingOptionError < StandardError; end

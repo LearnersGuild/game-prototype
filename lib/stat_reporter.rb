6ffc234d1daf3e874021dedb2204313c2df3d536
Shereef Bishay <shereef@gmail.com>
2016-08-16 23:05:27 -0700

    adds teamplay stat

--- lib/stat_reporter.rb
+++ lib/stat_reporter.rb
@@ -27,6 +27,7 @@ class StatReporter
       stat_report[:avg_proj_qual] = stats.proj_quality_for_player(player_id: id)
       stat_report[:lrn_supp] = stats.learning_support(player_id: id)
       stat_report[:cult_cont] = stats.culture_contribution(player_id: id)
+      stat_report[:team_play] = stats.team_play(player_id: id)
       stat_report[:est_accuracy] = stats.estimation_accuracy(player_id: id)
       stat_report[:est_bias] = stats.estimation_bias(player_id: id)
       stat_report[:no_proj_rvws] = stats.no_proj_reviews(player_id: id)
@@ -74,6 +75,7 @@ class StatReporter
       avg_proj_qual: stats.proj_quality_for_player(opts),
       lrn_supp: stats.learning_support(opts),
       cult_cont: stats.culture_contribution(opts),
+      team_play: stats.team_play(opts),
       est_accuracy: stats.estimation_accuracy(opts),
       est_bias: stats.estimation_bias(opts),
       no_proj_rvws: stats.no_proj_reviews(opts)
@@ -90,6 +92,7 @@ class StatReporter
       avg_proj_qual: stats.proj_quality_for_player(opts),
       lrn_supp: stats.learning_support(opts),
       cult_cont: stats.culture_contribution(opts),
+      team_play: stats.team_play(opts),
       project_contrib: stats.project_contribution(opts),
       expected_contrib: stats.expected_contribution(opts),
       contrib_gap: stats.contribution_gap(opts),

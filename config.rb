d0ba61ad08debd56f1e8db284ef0c210847c2916
Tanner Welsh <tannerwelsh@gmail.com>
2016-08-19 15:12:57 -0400

    Simplify stat report generation command using env variable
    Note assumptions of folders in proj directory. Updated README

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
 ROOT_PATH = File.dirname(__FILE__)
 LIB_PATH = File.join(ROOT_PATH, 'lib')
+DATA_PATH = File.join(ROOT_PATH, 'data')
+REPORTS_PATH = File.join(ROOT_PATH, 'reports')
 
 $LOAD_PATH.unshift( File.join(LIB_PATH) )

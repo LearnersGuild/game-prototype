bfe202df44d7505a2bf9c07e28883bb2b9cc5087
Tanner Welsh <tannerwelsh@gmail.com>
2016-09-11 14:57:57 -0400

    New STATS_PATH constant for stats folder

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 ROOT_PATH = File.dirname(__FILE__)
 LIB_PATH = File.join(ROOT_PATH, 'lib')
 DATA_PATH = File.join(ROOT_PATH, 'data')
+STATS_PATH = File.join(ROOT_PATH, 'stats')
 REPORTS_PATH = File.join(ROOT_PATH, 'reports')
 
 $LOAD_PATH.unshift( File.join(LIB_PATH) )
